eebb75c447a86ebff4307f02b270dd134d7c3639
in-jun <injuninjune@gmail.com>
2025-02-11 00:32:11 +0900

    chore: Add PostgreSQL driver
    Add PostgreSQL driver dependency to support
    PostgreSQL database migrations.
    
    - Add `github.com/lib/pq` to go.mod
    - Update go.sum accordingly

--- internal/pkg/db/mysql/mysql.go
+++ internal/pkg/db/mysql/mysql.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/urlpick/urlpick-api/internal/pkg/config"
 )
 
