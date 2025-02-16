4297e5d934e63bbb24767e041ea16f06e4544b8a
in-jun <injuninjune@gmail.com>
2025-02-16 20:45:52 +0900

    feat: Add Turnstile integration
    Add Turnstile secret key to the configuration.
    
    This allows for integrating Cloudflare Turnstile to protect
    against bot submissions.

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -3,25 +3,27 @@ package config
 import "os"
 
 type Config struct {
-	AppPort       string
-	MySQLHost     string
-	MySQLPort     string
-	MySQLDatabase string
-	MySQLUser     string
-	MySQLPassword string
-	BaseURL       string
+	AppPort            string
+	MySQLHost          string
+	MySQLPort          string
+	MySQLDatabase      string
+	MySQLUser          string
+	MySQLPassword      string
+	BaseURL            string
+	TurnstileSecretKey string
 }
 
 var AppConfig Config
 
 func Load() {
 	AppConfig = Config{
-		AppPort:       os.Getenv("APP_PORT"),
-		MySQLHost:     os.Getenv("MYSQL_HOST"),
-		MySQLPort:     os.Getenv("MYSQL_PORT"),
-		MySQLDatabase: os.Getenv("MYSQL_DATABASE"),
-		MySQLUser:     os.Getenv("MYSQL_USER"),
-		MySQLPassword: os.Getenv("MYSQL_PASSWORD"),
-		BaseURL:       os.Getenv("BASE_URL"),
+		AppPort:            os.Getenv("APP_PORT"),
+		MySQLHost:          os.Getenv("MYSQL_HOST"),
+		MySQLPort:          os.Getenv("MYSQL_PORT"),
+		MySQLDatabase:      os.Getenv("MYSQL_DATABASE"),
+		MySQLUser:          os.Getenv("MYSQL_USER"),
+		MySQLPassword:      os.Getenv("MYSQL_PASSWORD"),
+		BaseURL:            os.Getenv("BASE_URL"),
+		TurnstileSecretKey: os.Getenv("TURNSTILE_SECRET_KEY"),
 	}
 }
